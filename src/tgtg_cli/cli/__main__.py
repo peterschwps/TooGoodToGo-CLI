d1c295a558e111473c53048a06dd4c31dc2471d4
Peter Schwips <tgtg-cli@peterschwps.com>
2026-05-14 21:12:15 +0200

    docs: removed TODOs

--- src/tgtg_cli/cli/__main__.py
+++ src/tgtg_cli/cli/__main__.py
@@ -13,7 +13,3 @@ def main() -> None:
 
 if __name__ == "__main__":
     main()
-
-# TODOs:
-# TODO: in Docs vermerken --> dass Tools wie z.B. CleanMyMac den Cache und
-#       damit auch die Login Session löschen
